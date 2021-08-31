47a004c66727d94ba71428b8a15bdee215727eca
ayumi_miyazaki <a.miyazaki313@gmail.com>
2021-08-31 18:05:12 +0900

    [Update]入力文字の変更

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 var p = document.getElementById('text');
 var textLists = [
-  'Hello World',
-  'This is my App',
-  'How are you?',
-  'Hello Hello'
+  'arigato',
+  'ohayo',
+  'konbanwa',
+  'oyasuminasai'
 ];
 var checkTexts = [];
 
